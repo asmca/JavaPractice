5c4eaa6df5143164b2cd2e86e7ec56c2b920b220
asmca <sinosuse@gmail.com>
2014-11-23 14:14:16 +0800

    first submit on chapter6. testing.

---
+++ src/jdk7notes/chapter6/SwordsMan.java
@@ -0,0 +1,14 @@
+package jdk7notes.chapter6;
+
+/**
+ * Created by suse on 11/23/14.
+ */
+public class SwordsMan {
+    private String name;
+    private int level;
+    private int blood;
+
+    public void fight(){
+        System.out.println("挥动");
+    }
+}
