75f46751aeb02e27e32ee94bdb54e311280765e4
asmca <sinosuse@gmail.com>
2014-11-23 14:27:17 +0800

    test  commit

--- src/jdk7notes/chapter6/SwordsMan.java
+++ src/jdk7notes/chapter6/SwordsMan.java
@@ -9,6 +9,15 @@ public class SwordsMan {
     private int blood;
 
     public void fight(){
-        System.out.println("挥动");
+        System.out.println("挥剑攻击");
     }
+
+    public int getBlood(){
+        return blood;
+    }
+
+    public void setBlood(int blood){
+        this.blood=blood;
+    }
+
 }
