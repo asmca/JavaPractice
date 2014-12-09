f2da215447fa3e223ee898b2bcb178c918915aa7
asmca <zip@asmca.net>
2014-12-09 23:48:51 +0800

    chapter 8. 1209 update

---
+++ src/jdk7notes/chapter8/P_1.java
@@ -0,0 +1,16 @@
+package jdk7notes.chapter8;
+
+/**
+ * file in jdk7notes.chapter8
+ * Created by suse on 14-12-9.
+ */
+public class P_1 {
+    public static void main(String[] args) {
+        try {
+            int number=Integer.parseInt(args[0]);
+            System.out.println(number++);
+        }catch (NumberFormatException ex){
+            System.out.println("必须输入数字");
+        }
+    }
+}
---
+++ src/jdk7notes/chapter8/P_2.java
@@ -0,0 +1,14 @@
+package jdk7notes.chapter8;
+
+
+/**
+ * file in jdk7notes.chapter8
+ * Created by suse on 14-12-9.
+ */
+public class P_2 {
+    public static void main(String[] args) {
+        Object[] objs={"Java","7"};
+        Integer number=(Integer) objs[1];
+        System.out.print(number);
+    }
+}
