ee214d7e26b3ea13e584f42fcf44335edc3a7a9a
asmca <zip@asmca.net>
2014-12-28 14:27:24 +0800

    chapter 9. 1229 update
    Map. TreeMap . EntrySet()

--- src/jdk7notes/chapter9/MapKeyValue.java
+++ src/jdk7notes/chapter9/MapKeyValue.java
@@ -15,19 +15,20 @@ public class MapKeyValue {
         map.put("one", "一");
         map.put("two", "二");
         map.put("three", "三");
-
-        System.out.println("显示键");
-        foreach(map.keySet());
-
-        System.out.println("显示值");
-        foreach(map.values());
+//
+//        System.out.println("显示键");
+//        foreach(map.keySet());
+//
+//        System.out.println("显示值");
+//        foreach(map.values());
+        foreach(map.entrySet());
 
 
     }
 
-    public static void foreach(Iterable<String> iterable){
-        for (String element : iterable){
-            System.out.println(element);
+    public static void foreach(Iterable<Map.Entry<String,String>> iterable){
+        for (Map.Entry<String,String> element : iterable){
+            System.out.printf("(键 %s, 值 %s )", element.getKey(), element.getValue());
         }
     }
 
