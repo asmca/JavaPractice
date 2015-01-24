d56009d194d6a05fd0ce2140654be6ae8b6470f4
asmca <zip@asmca.net>
2015-01-25 00:12:30 +0800

    Commit for local changes.

--- src/jdk7notes/chapter14/ConnectionDemo.java
+++ src/jdk7notes/chapter14/ConnectionDemo.java
@@ -1,8 +1,25 @@
 package jdk7notes.chapter14;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * file in jdk7notes.chapter14
  * Created by suse on 15-1-13.
  */
 public class ConnectionDemo {
+    public static void main(String[] args)
+        throws ClassNotFoundException, SQLException{
+
+        Class.forName("com.mysql.jdbc.Driver");
+        String jdbcURL = "jdbc:mysql://localhost:3306/lxr";
+
+        String user = "root";
+        String password = "";
+
+        try (Connection conn = DriverManager. getConnection(jdbcURL, user, password)){
+            System.out.printf("数据库已经 %s%n", conn.isClosed() ? "关闭" : "打开");
+        }
+    }
 }
