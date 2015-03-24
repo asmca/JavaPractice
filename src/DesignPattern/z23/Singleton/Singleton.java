07d7804319bbfa62a78df9d76664037845df09e8
Suse Shi <zip@asmca.net>
2015-03-24 12:55:45 +0800

    singleton pattern 3

---
+++ src/DesignPattern/z23/Singleton/Singleton.java
@@ -0,0 +1,29 @@
+package DesignPattern.z23.Singleton;
+
+/**
+ * Created by suse on 15-3-24.
+ */
+public class Singleton {
+    private  static Singleton instance = null;
+
+    private Singleton(){
+
+    }
+
+    public static Singleton getInstance(){
+        if (instance ==null)
+        {
+            synchronized (instance) {
+             if (instance==null){
+                instance = new Singleton();
+             }
+            }
+        }
+        return instance;
+    }
+
+    public  Object readResolve(){
+        return instance;
+    }
+
+}
