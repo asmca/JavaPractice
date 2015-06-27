fa570f133538540a561449fdd51d2c87ee647746
asmca <zip@asmca.net>
2015-06-27 15:18:46 +0800

    0626 update.

---
+++ src/javabook/puzzlers/q2.java
@@ -0,0 +1,17 @@
+package javabook.puzzlers;
+
+/**
+ * file in java.puzzlers
+ * Created by suse on 15-6-23.
+ */
+public class q2 {
+    public static void main(String[] args) {
+
+        System.out.println(2.00 - 1.10);
+
+        System.out.printf("%.2f%n",  2.00-1.10);
+
+
+
+    }
+}
